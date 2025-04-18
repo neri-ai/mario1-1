77df4d31cf80c6dc610c2c4fb4e1cf4ca2b87a2e
根來 龍志 <r_negoro@marksnet.co.jp>
2025-04-18 19:16:20 +0900

    add

--- game.js
+++ game.js
@@ -1,6 +1,8 @@
 import { Mario } from "./Player/mario.js";
 import { drawGround } from "./Blocks/drawGround.js";
 import { inputKey } from "./input/inputKey.js";
+import { normalBlock } from "./Blocks/normalBlock.js";
+import { collisionDetection } from "./Blocks/normalBlock.js";
 
 // canvasの取得
 const canvas = document.querySelector("canvas");
@@ -18,9 +20,15 @@ const mario = new Mario(150, 100, 60, 60);
 // 入力値の取得
 const input = inputKey();
 
+// ブロックのインスタンスを格納する変数
+let normalBlocks = [];
+
 // ゲームロジックの実装
 function init() {
-        
+        // normalBlockのインスタンスを生成
+        normalBlocks = [
+                new normalBlock(840, 720, 60, 60),
+        ];
 }
 
 // ゲームのメインループ
@@ -33,7 +41,14 @@ function gameLoop() {
         
         mario.update(c, input);
         mario.draw(c);
+        normalBlocks.forEach((block) => {
+                block.draw(c);
+                block.update(c);
+        });
+
         drawGround(c, canvas);
+
+        collisionDetection(mario, normalBlocks);
 }
 
 // ゲームの開始
